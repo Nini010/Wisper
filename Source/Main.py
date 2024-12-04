9db1d6c2489ec07af8c0a0fa3af90e6a96c94933
nini010 <aleena.bakhshi@gmail.com>
2024-12-04 10:06:42 +0500

    Pro

--- Source/Main.py
+++ Source/Main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app=Flask(__name__) 
 
-@app.route('/DM')
+@app.route('/dm')
 def DM():
     return render_template('DMs.html')
 
@@ -14,6 +14,21 @@ def chats():
 def settings():
     return render_template('settings.html')
     
+@app.route('/search')
+def search():
+    return render_template('Search.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('EditProfile.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('SignIn.html')
+
+@app.route('/addpost')
+def addPost():
+    return "This is the Add Post page (placeholder)."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
